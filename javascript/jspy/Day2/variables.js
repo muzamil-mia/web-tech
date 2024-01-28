87f65b194a79c8e3faa2ca94122a44eedcff81c1
muzamil-mia <hussainmuzamil862@gmail.com>
2024-01-28 23:07:31 +0530

    nothing to commit

--- javascript/jspy/Day2/variables.js
+++ javascript/jspy/Day2/variables.js
@@ -5,4 +5,27 @@ function hello() {
   console.log(x);
   console.log(window);
 }
+let b;
+console.log(b);
+// console.log(c);
+
+//const cannot be re-initialized
+// const c = 100;
+// c = 10;  //typeerror: assignment to constant variable 
+// console.log(c);
 hello();
+
+//var can be re-declared and re-initialized
+var a = 100;
+
+//let cannot be re-declared and re-initialized
+// let l = 10;
+// let l = 20; //syntax error identifier l has already been declared
+
+//const cannot be re-declared and re-initialized
+// const c = 10;
+// const c = 20; //syntax error c identifier is already been declared
+
+ x = 100;
+console.log(x);
+console.log(window);
--- javascript/modern-javascript/day4/functions.js
+++ javascript/modern-javascript/day4/functions.js
@@ -100,6 +100,13 @@ showMessage("hello");
 
 //in js a default parameter is evaluated every time the function is called without the respective parameter.
 //in the example below anotherFunction() isn't called at all if the from 
+
+// function anotherFunction(){
+//     alert("hello");
+// }
+//if from parameter is provided by method calling statement anotherFunction() will not be called at all
+//on the other hand, it's independently called every time when from parameter is missing
+/*
 function showMessage(message, from = anotherMessage()) {
     alert(message +": "+from);
 }
@@ -107,4 +114,215 @@ function showMessage(message, from = anotherMessage()) {
 
 // showMessage("hello");//no output
 // showMessage("hello", "muzamil"); //hello muzamil
-showMessage("hello", undefined); // no output
+//showMessage("hello", undefined); // no output
+showMessage("muzammil")
+*/
+
+/*
+function showMessage(from, text) {
+    if(text == undefined) {
+        text = 'no text given';
+    }
+
+    alert(from + ": " + text);
+}
+
+showMessage("muzamil");
+*/
+
+/*
+function showMessage(text) {  //if the parameter is missing
+    // if(text == undefined) {
+    //     text = 'empty message';
+    // }
+
+    //or
+
+    text = text || "empty text";
+
+    alert(text);
+}
+
+showMessage();  //empty message
+*/
+
+/*
+function showCount(count) {
+    //if count is undefined or null, show "unknown"
+
+    alert(count ?? "unknown");
+
+}
+*/
+
+//showCount(0); //0
+/*
+showCount(null);//unknown
+showCount()//unknown
+*/
+
+//Returning a value
+/*
+function sum(a, b) {
+    return a+b;
+}
+
+let result = sum(1,3);
+alert(result); //3
+*/
+
+//a function with empty return or without it returns undefined
+/*
+function doNothing() {
+   
+}
+
+//alert( doNothing() === undefined);//true
+alert(doNothing());
+*/
+
+/*
+function doNothing(){
+    return;
+}
+alert(doNothing());
+
+return
+    "hello how are you"
+
+    //the above return statement is same as below
+    return;
+    "hello how are you";
+    //because js assumes semicolon after return statement
+
+    //do like below
+    return(
+        "hello how are you"
+    )
+    */
+
+    /*
+    function showPrimes(n) {
+        nextPrime: for (let i = 2; i < n; i++) {
+            for(let j = 2; j < i; j++) {
+                if(i % j == 0) {
+                    continue nextPrime;
+                }
+            }
+            alert(i);
+        }
+    }
+    showPrimes(10);
+    */
+
+    //break the function
+    /*
+    function showPrimes(n) {
+        for(let i = 2; i < n; i++) {
+            if(! isPrime(i) ) continue;
+
+            alert(i);
+        }
+    }
+
+    function isPrime(n) {
+        for(let i = 2; i < n; i++){
+            if(n % i == 0) return false;
+        }
+        return true;
+    }
+
+showPrimes(10);
+*/
+
+/*
+function confirm(){
+    return false;
+}
+
+function checkAge(age) {
+    // if(age > 18) {
+    //     return true;
+    // }else{
+    //     return confirm("did parents allow you");
+    // }
+    
+   // return (age > 18) ? true : confirm("did your parents allow you");
+
+    //or
+    return (age > 18) || confirm ("same");
+}
+*/
+/*
+alert(checkAge(19));
+
+function checkMin(a,b){
+   return (a < b) ? a : b;
+}
+
+alert(checkMin(10,20));
+*/
+
+/*
+function power(x, y) {
+    let ans = 1;
+    for( i = 1; i <= y; i++) {
+        ans = ans * x;
+    }
+    return ans;
+}
+
+//alert(power(3,2));
+let x = prompt("x?", '');
+let y = prompt("y?", '');
+
+if(y < 1) {
+    alert(`power ${y} is not supported, use a positive value`);
+}else {
+    alert(power(x,y));
+}
+*/
+
+/*
+let a = 10;
+console.log(a);
+var b = 20;
+*/
+
+/*
+{
+    var a = 10;
+    let b = 20;
+    const  c = 20;
+}
+
+var a = 100;
+{
+    var a = 10;//samee name as global var
+    let b = 20;
+    const c = 30;
+    console.log(a); //10
+    console.log(b); //20
+    console.log(c); //30
+}
+console.log(a);//10
+*/
+
+/*
+let b = 200;
+{
+    var a = 10;
+    let b = 20;
+    const c = 30;
+    console.log(b);//20
+}
+console.log(b);//200
+*/
+
+const c = 100;
+function d() {
+    const c = 200;
+    console.log(c);
+}
+d();
+console.log(c);
